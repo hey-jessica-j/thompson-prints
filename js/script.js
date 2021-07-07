c5a194ad95e65bd2d6fff2519ff13eb6f08f4311
Jessica Jennings <75150881+hey-jessica-j@users.noreply.github.com>
2021-07-06 21:13:08 -0400

    redid typeing function

--- js/script.js
+++ js/script.js
@@ -1,72 +1,55 @@
 
 const body = document.querySelector("body");
 const main = document.querySelector("main");
-const quoteTextElement = document.getElementById("quote-text");
+
+//Navigation Elements
 const homeElement = document.getElementById("home");
 const shopElement = document.getElementById("shop");
 const makeTeeElement = document.getElementById("make-tee");
 const aboutElement = document.getElementById("about");
 const contactElement = document.getElementById("contact");
 
+//
+const sentenceElement = document.getElementById("sentence");
+
 const worldText = "It's your world ...";
 const creativityText = "Let your creativity run free. . . ";
-let speed = 150;
-let textAdded = false;
+let delay = 100;
 
-const typeWriterEffect = async function(element, text, textDelete) {
-  let i = -1;
-
-  const addText = setInterval( function() {
-    if (i< text.length) {
-      element.append(text.charAt(i))
-      i++;
-      checkIfDelete(element, text, textDelete);
-
-    }
-  }, speed)
-};
-
-const checkIfDelete = async function(element, text, textDelete) {
-  if (textDelete == true) {
-    let textLen = text.length;
-    let elementLen = element.innerText.length;
-    if (elementLen === textLen) {
-      await sleep(2000);
-      deleteTextEffect(element, text);
-    }
+const typeSentence = async function (text, element) {
+  const letters = text.split("");
+  let i = 0;
+  while (i < letters.length) {
+    await sleep(delay);
+    element.append(letters[i]);
+    //element.append(letters[i]);
+    i++
   }
-
+  return;
 };
 
-const deleteTextEffect = async function(element, text) {
-  //const delete = setInterval
-  let oldText = element.innerText;
-  let length = element.innerText.length;
-  let textDeleted
-  let i = length ;
-  console.log(oldText, length, i);
-
-  const time = setInterval( function() {
-    if(i >= 1) {
-      i--;
-      let newText = oldText.substring(0, i);
-      element.innerText = newText;
-
-      }
-    }, 100)
-
-};
+const deleteSentence = async function (element) {
+  const text = element.innerText;
+  const letters = text.split("");
+  let i = 0;
+  while (letters.length > 0) {
+    await sleep(delay);
+    letters.pop();
+    element.innerText = letters.join("");
+  }
+}
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 };
 
-const updateHomePage = async function() {
-  main.classList.add("home-picture");
-  typeWriterEffect(quoteTextElement, worldText, true);
-
-  await sleep(9000);
-
-  typeWriterEffect(quoteTextElement, creativityText, false);
+const typeQuoteSentence = async function () {
+  await sleep(1000);
+  typeSentence(worldText, sentenceElement);
+  await sleep(3000);
+  deleteSentence(sentenceElement);
+  await sleep(4000);
+  typeSentence(creativityText, sentenceElement);
 };
-body.onLoad = updateHomePage();
+
+typeQuoteSentence();
