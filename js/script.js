a350dd8d5b73a5dd984933949f1c6748bf735c2d
Jessica Jennings <75150881+hey-jessica-j@users.noreply.github.com>
2021-07-07 15:24:20 -0400

    separating JavaScript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//import {typeSentence, deleteSentence} from "./typingEffect.js";
+import {typeSentence, deleteSentence} from "./typingEffect.js";
 
 
 const body = document.querySelector("body");
@@ -11,40 +11,6 @@ const makeTeeElement = document.getElementById("make-tee");
 const aboutElement = document.getElementById("about");
 const contactElement = document.getElementById("contact");
 
-
-
-//Creates the typing headline in document
-const sentenceElement = document.getElementById("sentence");
-const worldText = "It's your world";
-const creativityText = "Let your creativity run free ";
-let delay = 100;
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-};
-
-const typeSentence = async function (text, element) {
-  const letters = text.split("");
-  let i = 0;
-  while (i < letters.length) {
-    await sleep(delay);
-    element.append(letters[i]);
-    //element.append(letters[i]);
-    i++
-  }
-  return;
-};
-
-const deleteSentence = async function (element) {
-  const text = element.innerText;
-  const letters = text.split("");
-  let i = 0;
-  while (letters.length > 0) {
-    await sleep(delay);
-    letters.pop();
-    element.innerText = letters.join("");
-  }
-};
 
 const typeQuoteSentence = async function () {
   await sleep(1000);
